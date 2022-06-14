3977278173cac9eac72ea403da948fd6ed16de83
dimitar.nizamov <dimitur2204@gmail.com>
2022-06-14 14:13:10 +0200

    Change local storage of the hasViewedDialog to session storage

--- scripts/modal.js
+++ scripts/modal.js
@@ -5,9 +5,9 @@ dialog.on("show", function (dialogEl, triggerEl) {
 	console.log(dialogEl);
 	console.log(triggerEl);
 });
-if (localStorage.getItem("hasViewedDialog") !== "true") {
+if (sessionStorage.getItem("hasViewedDialog") !== "true") {
 	setTimeout(() => {
 		dialog.show();
-		localStorage.setItem("hasViewedDialog", "true");
+		sessionStorage.setItem("hasViewedDialog", "true");
 	}, 1000);
 }
