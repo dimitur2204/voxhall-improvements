4c16e182357a0037e50050ec2915bf0305a23c67
dimitar.nizamov <dimitur2204@gmail.com>
2022-06-14 14:17:41 +0200

    Remove console logs

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,6 @@
 var dialogEl = document.getElementById("my-dialog");
 var dialog = new A11yDialog(dialogEl);
 
-dialog.on("show", function (dialogEl, triggerEl) {
-	console.log(dialogEl);
-	console.log(triggerEl);
-});
 if (sessionStorage.getItem("hasViewedDialog") !== "true") {
 	setTimeout(() => {
 		dialog.show();
